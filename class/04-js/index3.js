3bf8ac0ad970514a19765cba3aa4098f7f6eaed6
geosuk <geosuk9808@gmail.com>
2024-05-27 18:59:26 +0900

    js 수업

---
+++ class/04-js/index3.js
@@ -0,0 +1,35 @@
+
+// #1.var
+var a; // 변수 선언
+console.log(a);
+a= 2;
+console.log(a);
+var a2 = 4;
+console.log(a2+a);
+
+var a2 = 6;
+console.log(a2);
+
+// #2. let
+let b;
+console.log(b);
+b= 7;
+console.log(b);
+
+let b2 = 77;
+console.log(b2);
+// let b2 = 89;
+
+b2= 78;
+console.log(b2);
+
+
+
+// #3. const
+const c = 3;
+console.log(c);
+// c = 33; error
+// var x; << 변수 선언이 자동으로 되는 걸 호이스팅이라고 한다
+console.log(x); // undefined
+var x = 10;
+console.log(x); // 10
